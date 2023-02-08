b783bf86ecf578b897d8c869daba17e3b24b3868
lpodev <lpodevco@gmail.com>
2023-02-08 09:24:21 +0100

    Format

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -4,61 +4,53 @@ const EmptyCartException = require("./EmptyCartException.js");
 const UpdateCartException = require("./UpdateCartException.js");
 
 module.exports = class Cart {
+  //region private attributes
+  #items = [];
+  //endregion private attributes
 
-    //region private attributes
-    #items = [];
-    //endregion private attributes
+  //region public methods
+  constructor(items) {
+    this.#items = items;
+  }
 
-    //region public methods
-    constructor(items) {
-        this.#items = items;
+  get items() {
+    if (this.#items === null) {
+      throw new EmptyCartException("Cart must have at least one item");
     }
+    return this.#items;
+  }
 
-    get items() {
-        if (this.#items === null) {
-            throw new EmptyCartException("Cart must have at least one item");
-        }
-
-        return this.#items;
+  get total() {
+    if (this.#items === null) {
+      throw new EmptyCartException("Cart must have at least one item");
     }
-
-    get total() {
-        if (this.#items === null) {
-            throw new EmptyCartException("Cart must have at least one item");
-        }
-
-        let total = 0;
-        for (let item of this.#items) {
-            total += item.quantity * item.price;
-        }
-        return total;
+    let total = 0;
+    for (let item of this.#items) {
+      total += item.quantity * item.price;
     }
-    //endregion public methods
+    return total;
+  }
+  //endregion public methods
 
-    //region private methods
-    count(bool = false) {
-        if (this.#items === null) {
-            throw new EmptyCartException("Cart must have at least one item");
-        }
-        if (bool) {
-            return this.#items.length;
-        }
-        let count = 0;
-            for (let item of this.#items) {
-                count += item.quantity;
-            }
-        return count;
+  //region private methods
+  count(bool = false) {
+    if (this.#items === null) {
+      throw new EmptyCartException("Cart must have at least one item");
+    } else if (bool) {
+      return this.#items.length;
     }
-
-    add(items) {
-        if (items === null) {
-            throw new UpdateCartException("Cart must have at least one item");
-        }
-        this.#items = items;
+    let count = 0;
+    for (let item of this.#items) {
+      count += item.quantity;
     }
+    return count;
+  }
 
-    //endregion private methods
-}
-
-
-
+  add(items) {
+    if (items === null) {
+      throw new UpdateCartException("Cart must have at least one item");
+    }
+    this.#items = items;
+  }
+  //endregion private methods
+};
