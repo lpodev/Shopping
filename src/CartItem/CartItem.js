3e92c01b82a4d723a0ca151c0a216151f35f3bad
lpodev <lpodevco@gmail.com>
2023-02-08 08:45:13 +0100

    CartItem tests all passed

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -15,35 +15,54 @@ module.exports = class CartItem {
 
     //region public methods
     constructor(articleId, name, quantity, price) {
-        throw new Error();
+        if (articleId < 1) {
+            throw new InvalidArticleIdException("Article id must be greater than 0");
+        }
+        if (quantity < 1) {
+            throw new InvalidQuantityException("Quantity must be greater than 0");
+        }
+        if (price < 10) {
+            throw new InvalidPriceException("Price must be greater than 10");
+        }
+
+        this.#articleId = articleId;
+        this.#name = name;
+        this.#quantity = quantity;
+        this.#price = price;
     }
 
     get articleId() {
-        throw new Error();
+        return this.#articleId;
     }
 
     get name() {
-        throw new Error();
+        return this.#name;
     }
 
     get quantity() {
-        throw new Error();
+        return this.#quantity;
     }
 
     set quantity(value) {
-        throw new Error();
+        if (value < 1) {
+            throw new InvalidQuantityException("Quantity must be greater than 0");
+        }
+        return this.#quantity = value;
     }
 
     get price() {
-        throw new Error();
+        return this.#price;
     }
 
     set price(value) {
-        throw new Error();
+        if (value < 10) {
+            throw new InvalidPriceException("Price must be greater than 10");
+        }
+        return this.#price = value;
     }
 
     get total() {
-        throw new Error();
+        return this.#quantity * this.#price;
     }
     //endregion public methods
 
